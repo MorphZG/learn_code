e03cd195b3642e8e7cc7d3f6147a0ce2be6f8ba7
MorphZG <zoran1607@gmail.com>
2023-07-15 04:49:01 +0200

    test configuration

--- app_brewery web_development_bootcamp/10 intermediate javascript/exercise.js
+++ app_brewery web_development_bootcamp/10 intermediate javascript/exercise.js
@@ -10,16 +10,66 @@ getRandom(50); // will return number from zero to 50 but not including 50
 // the random percentage and display it as a browser alert.
 
 function randomNumber(scale) {
-    let random = Math.floor(Math.random() * scale + 1);
-    return random;
+    let randomChance = Math.floor(Math.random() * scale + 1); // 1 to 100
+    return randomChance;
 }
-
 
 let personOne = prompt("name of person one");
 let personTwo = prompt("name of person two");
 
-let chance = randomNumber(100)
+let loveScore = randomNumber(100);
 
-alert("Your love score is " + chance + "%")
+alert(`Your love score is ${loveScore} %`);
+
+// adding conditional statements
+
+if (loveScore > 70) {
+    alert(`Your love score is ${loveScore}%. You are real soul mates`);
+}
+if (loveScore > 30 && loveScore <= 70) {
+    alert(`Your love score is ${loveScore}%`);
+}
+if (loveScore <= 30) {
+    alert(
+        `Your love score is ${loveScore}%. You go to together like oil and water.`
+    );
+} else {
+    alert(`Your love score is ${loveScore}%. Not bad`);
+}
+
+// exercise: BMI calculator advanced
+/*
+Previously, we've created a function that is able to calculate the BMI. But once we get a result, we will want to tell the user what the number means.
+Write a function that outputs (returns) a different message depending on the BMI.
+BMI <18.5, the output should be: "Your BMI is <bmi>, so you are underweight."
+BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight."
+BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight."
+The message MUST be returned as an output from your function. You should NOT NEED to use alert, prompt or console.log in this challenge.
+IMPORTANT the message wording has to match precisely for the code to pass the validation. Including punctuation and capitalisation.
+*/
+
+function bmiCalculator(weight, height) {
+    let bmi;
+    let interpretation;
+
+    bmi = weight / (height ** 2);
+    
+    console.log(`Your BMI is ${bmi}`);
+
+    if (bmi < 18.5) {
+        interpretation = `Your BMI is ${bmi}, so you are underweight.`;
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = `Your BMI is ${bmi}, so you have normal weight.`;
+    } else {
+        interpretation = `Your BMI is ${bmi}, so you are overweight.`;
+    }
+    
+    return interpretation;
+}
+
+bmiCalculator(65, 1.78);
+
+// challange, leap years
+
 
 //tags: intermediate javascript
