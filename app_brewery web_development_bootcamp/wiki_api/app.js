8758d569f4f0d52af2df2ee2ca8d71c80f71f9b5
MorphZG <zoran1607@gmail.com>
2024-03-08 13:50:46 +0100

    project setup

--- app_brewery web_development_bootcamp/wiki_api/app.js
+++ app_brewery web_development_bootcamp/wiki_api/app.js
@@ -22,6 +22,26 @@ const articles = { title: String, content: String };
 // model
 const Article = mongoose.model("Article", articles);
 
+//--------------------
+// route handlers
+//--------------------
+
+app.get("/articles", async (req, res) => {
+    await Article.find({}).then((foundArticles) => {
+        console.log(foundArticles);
+        res.send(foundArticles);
+    });
+});
+
+app.post("/articles", async (req, res) => {
+    let newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+    });
+
+    await newArticle.save();
+});
+
 // ----------------
 // listener
 //-----------------
