1bddbf3a4ba20e7b76beac91ed3591b9319e701c
MorphZG <zoran1607@gmail.com>
2024-03-17 12:07:59 +0100

    update progress

--- app_brewery web_development_bootcamp/wiki_api/app.js
+++ app_brewery web_development_bootcamp/wiki_api/app.js
@@ -10,10 +10,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // -----------------------
-// route handlers
-// -----------------------
-
-// -----------------------
 // database configuration
 // -----------------------
 mongoose.connect("mongodb://localhost:27017/wikiTutorial");
@@ -26,29 +22,75 @@ const Article = mongoose.model("Article", articles);
 // route handlers
 //--------------------
 
-app.get("/articles", async (req, res) => {
-    try {
-        let foundArticles = await Article.find({});
-        console.log(foundArticles);
-        res.send(foundArticles);
-    } catch (error) {
-        console.log(error);
-    }
-});
+app.route("/articles")
 
-app.post("/articles", async (req, res) => {
-    let newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content,
+    .get(async (req, res) => {
+        try {
+            let foundArticles = await Article.find({});
+            res.send(foundArticles);
+            console.log("Articles found!");
+        } catch (err) {
+            console.log(err);
+        }
+    })
+
+    .post(async (req, res) => {
+        let newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        });
+        try {
+            await newArticle.save();
+            res.send(newArticle);
+            console.log("Article saved!");
+        } catch (err) {
+            console.log(err);
+        }
+    })
+
+    .delete(async (req, res) => {
+        try {
+            await Article.deleteMany({});
+            console.log("Articles delted!");
+        } catch (err) {
+            console.log(err);
+        }
     });
 
-    try {
-        await newArticle.save();
-        console.log("Article saved!");
-    } catch (err) {
-        console.log(err);
-    }
-});
+app.route("/articles/:title")
+    .get(async (req, res) => {
+        try {
+            let foundArticle = await Article.findOne({
+                title: req.params.title,
+            });
+            if (foundArticle) {
+                console.log("Article found!");
+                res.send(foundArticle);
+            } else {
+                console.log("Nothing found!");
+                res.send();
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    })
+    .delete(async (req, res) => {
+        try {
+            let dbResponse = await Article.deleteOne({
+                title: req.params.title,
+            });
+            res.send(dbResponse);
+            if (dbResponse.deletedCount > 0) {
+                console.log("Article deleted!");
+            } else {
+                console.log("Nothing found!");
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    })
+    .put(async (req, res) => {})
+    .patch(async (req, res) => {});
 
 // ----------------
 // listener
