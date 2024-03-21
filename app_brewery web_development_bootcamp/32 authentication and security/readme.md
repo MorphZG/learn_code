97c2a215a538cb2e355b3533429e8c3a734d47fe
MorphZG <zoran1607@gmail.com>
2024-03-21 20:52:28 +0100

    update progress

--- app_brewery web_development_bootcamp/32 authentication and security/readme.md
+++ app_brewery web_development_bootcamp/32 authentication and security/readme.md
@@ -4,6 +4,8 @@
 ## notes
 
 - Work directory: [secrets_app](../secrets_app/)
+- [mongoose plugins](https://mongoosejs.com/docs/plugins.html)
+- [mongoose-encryption](https://www.npmjs.com/package/mongoose-encryption) encryption and authentication package for mongoose documents
 
 ## summary
 
@@ -11,6 +13,17 @@ This module will cover different levels of security, from simple logins, creatin
 
 Web app i will try to replicate is a [whisper.sh](https://whisper.sh). With simple and only the basic features, that way we can focus on learning the security.
 
+### authentication levels
+
+- User can create account to login and access the app while data like username and password are stored in a remote database.
+- Next step is to encrypt the user data before storing it in a remote database.
+
 ## read more
+
+- [mongoose plugins](https://mongoosejs.com/docs/plugins.html)
+- [cryptii.com](https://cryptii.com/) encrypt/decrypt text using different methods
+- [youtube.com/video](https://www.youtube.com/watch?v=G2_Q9FoD-oQ) "Enigma Machine" by Numberphile
+- [youtube.com/video](https://www.youtube.com/watch?v=V4V2bpZlqx8) "Flaw in the Enigma code" by Numberphile
+- [security.stackexchange/thread](https://security.stackexchange.com/questions/379/what-are-rainbow-tables-and-how-are-they-used) "What are rainbow tables and how to use them"
 
 ## demo credentials
--- app_brewery web_development_bootcamp/secrets_app/app.js
+++ app_brewery web_development_bootcamp/secrets_app/app.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const ejs = require("ejs");
+import express from "express";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+import "dotenv/config";
+import ejs from "ejs";
 
 const app = express();
 
@@ -8,15 +11,70 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine", "ejs");
 
+const encriptionString = process.env.secretString;
+
+// -----------------------
+// database configuration
+// -----------------------
+mongoose.connect("mongodb://localhost:27017/secretTutorial");
+
+const userSchema = new mongoose.Schema({ email: String, password: String });
+
+userSchema.plugin(encrypt, {
+    secret: encriptionString,
+    encryptedFields: "password",
+});
+
+const User = new mongoose.model("User", userSchema);
+
+// ----------------
+// route handlers
+// ----------------
 app.route("/").get((req, res) => {
     res.render("home");
 });
-app.route("/login").get((req, res) => {
-    res.render("login");
-});
-app.route("/register").get((req, res) => {
-    res.render("register");
-});
+app.route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post(async (req, res) => {
+        try {
+            let foundUser = await User.findOne({ email: req.body.username });
+            if (foundUser) {
+                if (foundUser.password == req.body.password) {
+                    console.log("Password is correct!");
+                    res.render("secrets");
+                } else {
+                    console.log("Wrong password!");
+                    res.redirect("/login");
+                }
+            } else {
+                console.log("No such user in a database!");
+                res.redirect("/login");
+            }
+        } catch (err) {
+            console.log(err);
+            res.send();
+        }
+    });
+
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post(async (req, res) => {
+        let newUser = new User({
+            email: req.body.username,
+            password: req.body.password,
+        });
+        try {
+            await newUser.save();
+            res.render("secrets");
+        } catch (err) {
+            console.log(err);
+            res.send();
+        }
+    });
 
 // listener
 app.listen(3000, () => {
