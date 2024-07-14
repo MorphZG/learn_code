a2ec1283ca71d00eb53081d7fdfadad51a7c59bc
MorphZG <zoran1607@gmail.com>
2024-07-14 18:30:45 +0200

    add javascript-exercises

---
+++ the_odin_project/javascript-exercises/03_reverseString/reverseString.js
@@ -0,0 +1,7 @@
+const reverseString = function (str) {
+    let reversed = [...str].reverse().join("");
+    return reversed;
+};
+
+// Do not edit below this line
+module.exports = reverseString;
---
+++ the_odin_project/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -0,0 +1,6 @@
+const removeFromArray = function (array, element) {};
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+
+// Do not edit below this line
+export default removeFromArray;
