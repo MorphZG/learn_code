0b71342c9b761e3dd9a41e0cdbd56a9e112b8149
MorphZG <83453128+MorphZG@users.noreply.github.com>
2026-05-28 16:34:55 +0200

    progress update

--- the_odin_project/foundations_course/DOM exercise/script.js
+++ the_odin_project/foundations_course/DOM exercise/script.js
@@ -18,24 +18,15 @@ h3.style.color = "blue";
 container.appendChild(h3);
 
 const secondContainer = document.createElement("div");
-secondContainer.textContent = "Hi I'm blue!";
-secondContainer.style.color = "blue";
+secondContainer.style.border = "black";
+secondContainer.style.backgroundColor = "brown";
 
-/*
-Add the following elements to the container using only javascript and the DOM methods:
-  - a `<p>` tag with red text that says "Hi I'm red!"
-  - an `<h3>` with blue text that says "I'm blue h3!"
-  - a `<div>` with black border and pink background color with the following elements inside:
-    - another `<h1>` that says "I'm in a div"
-    - a `<p>` that says "ME TOO!"
-    - Hint for this one: after creating the `<div>` with `createElement`, append the `<h1>` and `<p>` to it before adding it to the container.
+const h1 = document.createElement("h1");
+h1.textContent = "I am in aaaa div";
 
-## Events
+const secondParagraph = document.createElement("p");
+secondParagraph.textContent = "ME TOO!";
 
-Now that we have a handle on manipulating the DOM with javascript, the next step is learning how to make it happen dynamically or on demand! Events are how you make that magic happen on your pages. Events are actions that occur on your webpage, such as mouse-clicks or key-presses. Using javascript, we can make our webpage listen to and react to these events.
-
-There are three primary ways to go about this:
-  - You can specify function attributes directly on your HTML elements.
-  - You can set properties in the form of `on<eventType>`, such ad `onclick` or `onmousedown`, on the DOM nodes in your javascript.
-  - You can attach event listeners to the DOM nodes in your javascript.
-*/
+secondContainer.appendChild(h1);
+secondContainer.appendChild(secondParagraph);
+container.appendChild(secondContainer);
---
+++ the_odin_project/test.js
@@ -0,0 +1,6 @@
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+const upperCats = cats.map((cat) => cat.toUpperCase());
+
+console.log(upperCats);
+// [ "LEOPARD", "SERVAL", "JAGUAR", "TIGER", "CARACAL", "LION" ]
